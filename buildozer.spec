6f3b593fe5c6c3d2c1401d7a4022bb28a8b24960
lenkaliss <koshliakova@gmail.com>
2026-05-05 19:06:49 +0300

    Add files via upload

--- buildozer.spec
+++ buildozer.spec
@@ -1,7 +1,7 @@
 [app]
 
 # (str) Title of your application
-title = TOMS TEST LAB
+title = Image Test Lab
 
 # (str) Package name
 package.name = imagetest
@@ -12,64 +12,56 @@ package.domain = org.test
 # (str) Source code where the main.py live
 source.dir = .
 
-# (list) Source files to include
-# ДОБАВЛЕНО xml для file_paths.xml
-source.include_exts = py,png,jpg,jpeg,kv,ttf,txt,db,xml
+# (list) Source files to include (let empty to include all the files)
+source.include_exts = py,png,jpg,kv,atlas
 
-# (str) Application versioning
-version = 0.1.1
+# (str) Application versioning (method 1)
+version = 0.1
 
 # (list) Application requirements
-requirements = python3, kivy==2.3.0, kivymd==1.1.1, pillow, plyer, pyjnius, android, sharedstorage4kivy, https://github.com/Android-for-Python/gestures4kivy/archive/main.zip, https://github.com/Android-for-Python/camera4kivy/archive/main.zip
+# Добавлены прямые ссылки на архивы для camera4kivy и gestures4kivy
+requirements = python3, kivy==2.3.1, kivymd==1.1.1, pillow, pyjnius
+# (str) Presplash of the application
+presplash.filename = %(source.dir)s/assets/placeholder.png
+
+# (str) Icon of the application
+icon.filename = %(source.dir)s/assets/placeholder.png
 
 # (str) Supported orientations
 orientation = portrait
 
-# (bool) Indicate if the application should be fullscreen
+# (bool) Indicate if the application should be fullscreen or not
 fullscreen = 0
 
 # (list) Permissions
-android.permissions = READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE, CAMERA, MANAGE_EXTERNAL_STORAGE, READ_MEDIA_IMAGES
+# Максимальный набор прав для тестирования доступа к медиа
+android.permissions = READ_EXTERNAL_STORAGE, READ_MEDIA_IMAGES, WRITE_EXTERNAL_STORAGE
 
-# (int) Target Android API
+# (int) Target Android API, should be as high as possible.
 android.api = 33
 
-# (int) Minimum API
+# (int) Minimum API your APK will support.
 android.minapi = 21
 
-# (str) Android NDK version
+# (str) Android NDK version to use
 android.ndk = 25b
 
 # (bool) If True, then skip trying to update the Android sdk
-# ИСПРАВЛЕНО: используем 0 вместо False для совместимости с Python 3.12
-android.skip_update = 0
+android.skip_update = False
 
 # (bool) If True, then automatically accept SDK license
-# ИСПРАВЛЕНО: используем 1 вместо True
-android.accept_sdk_license = 1
+android.accept_sdk_license = True
 
-# (str) Android entry point
-android.entrypoint = org.kivy.android.PythonActivity
-
-# (list) Android application architectures
+# (str) The Android arch to build for, choices: armeabi-v7a, arm64-v8a, x86, x86_64
 android.archs = arm64-v8a
 
 # (bool) enables Android auto backup feature
-android.allow_backup = 1
-
-# --- СЕКЦИЯ ДЛЯ РАБОТЫ КАМЕРЫ И ГАЛЕРЕИ ---
-
-# Связываем файл путей (должен лежать в res/xml/file_paths.xml)
-android.add_xml = res/xml/file_paths.xml
-
-# Настройка провайдера файлов
-android.meta_data = androidx.core.content.FileProvider=android.support.v4.content.FileProvider
-
-# Gradle зависимости для CameraX (РЕШАЕТ КРАШ ПРИ КАСАНИИ)
-android.gradle_dependencies = "androidx.camera:camera-core:1.3.0", "androidx.camera:camera-camera2:1.3.0", "androidx.camera:camera-lifecycle:1.3.0", "androidx.camera:camera-video:1.3.0", "androidx.camera:camera-view:1.3.0", "androidx.camera:camera-extensions:1.3.0"
-
-# ------------------------------------------
+android.allow_backup = True
 
 [buildozer]
+
+# (int) Log level (0 = error only, 1 = info, 2 = debug (with command output))
 log_level = 2
+
+# (int) Display warning if buildozer is run as root (0 = off, 1 = on)
 warn_on_root = 1
--- main.py
+++ main.py
@@ -1,235 +1,180 @@
-from kivy.lang import Builder
-from kivymd.app import MDApp
-from kivy.utils import platform
-from kivy.properties import StringProperty
-from kivymd.uix.screen import MDScreen
 import os
-import shutil
+import io
+import time
+from kivy.lang import Builder
+from kivy.utils import platform
+from kivy.clock import mainthread
+from kivy.core.image import Image as CoreImage
+from kivymd.app import MDApp
+from kivy.logger import Logger
 
-# Системные импорты для Android
+# Нативные импорты
 if platform == 'android':
-    from jnius import autoclass, cast
+    from android.permissions import request_permissions, Permission
+    from jnius import autoclass
     from android import activity
-    from android.permissions import request_permissions, Permission, check_permission
-    
     PythonActivity = autoclass('org.kivy.android.PythonActivity')
     Intent = autoclass('android.content.Intent')
-    MediaStore = autoclass('android.provider.MediaStore')
-    Uri = autoclass('android.net.Uri')
-    File = autoclass('java.io.File')
-    FileProvider = autoclass('androidx.core.content.FileProvider')
-    
-    try:
-        from sharedstorage4kivy import SharedStorage
-    except ImportError:
-        SharedStorage = None
-
-from plyer import filechooser
+    FileOutputStream = autoclass('java.io.FileOutputStream')
 
 KV = '''
-MDScreen:
-    md_bg_color: 0.1, 0.1, 0.1, 1
+MDBoxLayout:
+    orientation: 'vertical'
+    
+    MDTopAppBar:
+        title: "Image Render Test Lab"
+        elevation: 4
 
-    MDBoxLayout:
-        orientation: 'vertical'
-        padding: "10dp"
-        spacing: "5dp"
-
-        # Предпросмотр выбранного изображения
-        FitImage:
-            id: display_img
-            source: "assets/placeholder.png"
-            size_hint: None, None
-            size: "220dp", "220dp"
-            pos_hint: {"center_x": .5}
-            radius: [20, ]
-
-        MDLabel:
-            id: status_label
-            text: "Тестовый стенд: Камера и Галерея"
-            halign: "center"
-            theme_text_color: "Custom"
-            text_color: 1, 1, 1, 1
-            font_style: "Caption"
+    ScrollView:
+        MDGridLayout:
+            cols: 1
             adaptive_height: True
+            padding: dp(15)
+            spacing: dp(20)
 
-        # Скролл, чтобы все кнопки влезли на экран
-        ScrollView:
-            do_scroll_x: False
-            MDBoxLayout:
+            # СПОСОБ 1: ПРЯМОЕ КОПИРОВАНИЕ ПОТОКА
+            MDCard:
                 orientation: 'vertical'
-                adaptive_height: True
-                spacing: "12dp"
-                padding: ["10dp", "10dp", "10dp", "20dp"]
+                size_hint_y: None
+                height: dp(250)
+                padding: dp(10)
+                MDLabel:
+                    text: "1. Native Stream Copy (File)"
+                    bold: True
+                    adaptive_height: True
+                Image:
+                    id: img_method_1
+                    source: ''
+                MDRaisedButton:
+                    text: "Выбрать (Stream)"
+                    on_release: app.open_gallery(method=1)
 
+            # СПОСОБ 2: CORE IMAGE (IN-MEMORY)
+            MDCard:
+                orientation: 'vertical'
+                size_hint_y: None
+                height: dp(250)
+                padding: dp(10)
+                MDLabel:
+                    text: "2. CoreImage (Bytes/Buffer)"
+                    bold: True
+                    adaptive_height: True
+                Image:
+                    id: img_method_2
                 MDRaisedButton:
-                    text: "📷 СДЕЛАТЬ ФОТО (Intent)"
-                    size_hint_x: 1
-                    md_bg_color: 0.2, 0.6, 0.2, 1
-                    on_release: app.test_camera_intent()
+                    text: "Выбрать (Buffer)"
+                    on_release: app.open_gallery(method=2)
 
+            # СПОСОБ 3: ПРЯМОЙ ПУТЬ (ДЛЯ ПРОВЕРКИ ОШИБОК ДОСТУПА)
+            MDCard:
+                orientation: 'vertical'
+                size_hint_y: None
+                height: dp(250)
+                padding: dp(10)
+                MDLabel:
+                    text: "3. Direct Path (RealPath)"
+                    bold: True
+                    adaptive_height: True
+                Image:
+                    id: img_method_3
                 MDRaisedButton:
-                    text: "1. SharedStorage (Copy to Cache)"
-                    size_hint_x: 1
-                    on_release: app.test_shared_storage()
-
-                MDRaisedButton:
-                    text: "2. Intent (GET_CONTENT) + Copy"
-                    size_hint_x: 1
-                    on_release: app.test_get_content()
-
-                MDRaisedButton:
-                    text: "3. Intent (ACTION_PICK) - No Copy"
-                    size_hint_x: 1
-                    on_release: app.test_action_pick()
-
-                MDRaisedButton:
-                    text: "4. Plyer Filechooser"
-                    size_hint_x: 1
-                    on_release: app.test_plyer_filechooser()
-                    
-                MDRaisedButton:
-                    text: "5. Camera4Kivy (choose_file)"
-                    size_hint_x: 1
-                    on_release: app.test_camera4kivy()
-                    
-                MDFlatButton:
-                    text: "ОЧИСТИТЬ КЭШ"
-                    text_color: 1, 0.3, 0.3, 1
-                    pos_hint: {"center_x": .5}
-                    on_release: app.clear_local_files()
+                    text: "Выбрать (Direct Path)"
+                    on_release: app.open_gallery(method=3)
 '''
 
-class TestApp(MDApp):
-    camera_path = StringProperty("")
+class ImageTestApp(MDApp):
+    current_method = 1
 
     def build(self):
         return Builder.load_string(KV)
 
     def on_start(self):
-        # Запрос всех необходимых разрешений при запуске [cite: 56]
         if platform == 'android':
             request_permissions([
-                Permission.CAMERA,
-                Permission.READ_EXTERNAL_STORAGE,
-                Permission.WRITE_EXTERNAL_STORAGE,
-                Permission.READ_MEDIA_IMAGES
+                Permission.READ_MEDIA_IMAGES,
+                Permission.READ_EXTERNAL_STORAGE
             ])
-            activity.bind(on_activity_result=self.on_android_result)
+            activity.bind(on_activity_result=self.on_handle_activity_result)
 
-    def test_camera_intent(self):
-        """Метод №6: Сделать фото и сохранить в папку приложения"""
+    def open_gallery(self, method):
+        self.current_method = method
         if platform == 'android':
-            if not check_permission(Permission.CAMERA):
-                self.root.ids.status_label.text = "Нет прав на камеру"
-                return
+            intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.setType("image/*")
+            PythonActivity.mActivity.startActivityForResult(intent, 101)
+        else:
+            print("Тест доступен только на Android")
 
-            intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            filename = f"photo_{int(os.times().elapsed)}.jpg"
-            save_path = os.path.join(self.user_data_dir, filename)
-            self.camera_path = save_path
-            
-            photo_file = File(save_path)
-            # Динамическое получение authority для FileProvider
-            package_name = PythonActivity.mActivity.getPackageName()
-            authority = f"{package_name}.fileprovider"
-            
-            uri = FileProvider.getUriForFile(PythonActivity.mActivity, authority, photo_file)
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, cast('android.os.Parcelable', uri))
-            PythonActivity.mActivity.startActivityForResult(intent, 4000)
+    @mainthread
+    def on_handle_activity_result(self, request_code, result_code, data):
+        if request_code == 101 and result_code == -1:
+            uri = data.getData()
+            if self.current_method == 1:
+                self.method_stream_copy(uri)
+            elif self.current_method == 2:
+                self.method_core_image(uri)
+            elif self.current_method == 3:
+                self.method_direct_path(uri)
 
-    def test_shared_storage(self):
-        """Метод №1: Современный способ через sharedstorage4kivy"""
-        if platform == 'android' and SharedStorage:
-            try:
-                ss = SharedStorage()
-                path = ss.copy_from_shared('Images')
-                if path:
-                    final_path = self.copy_file_to_app_folder(path)
-                    self.update_ui(final_path, "SharedStorage")
-            except Exception as e:
-                self.root.ids.status_label.text = f"SS Error: {e}"
-
-    def test_get_content(self):
-        """Метод №2: Выбор через системный Intent с копированием байтов"""
-        if platform == 'android':
-            intent = Intent(Intent.ACTION_GET_CONTENT).setType("image/*")
-            PythonActivity.mActivity.startActivityForResult(intent, 2000)
-
-    def test_action_pick(self):
-        """Метод №3: Выбор из галереи без копирования (прямой URI)"""
-        if platform == 'android':
-            intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            PythonActivity.mActivity.startActivityForResult(intent, 3000)
-
-    def test_plyer_filechooser(self):
-        """Метод №4: Классический Plyer"""
-        filechooser.open_file(on_selection=lambda x: self.update_ui(x[0], "Plyer") if x else None)
-
-    def test_camera4kivy(self):
-        """Метод №5: Выбор файла через библиотеку Camera4Kivy"""
-        if platform == 'android':
-            try:
-                from camera4kivy import Preview
-                Preview().choose_file(on_selection=lambda x: self.update_ui(x, "C4K"))
-            except Exception as e:
-                self.root.ids.status_label.text = f"C4K Error: {e}"
-
-    def on_android_result(self, requestCode, resultCode, intent):
-        """Обработка ответов от всех системных окон Android"""
-        if resultCode == -1: # RESULT_OK
-            if requestCode == 4000: # Камера
-                self.update_ui(self.camera_path, "Camera Capture")
-            elif requestCode == 2000: # Галерея (копирование)
-                if intent and intent.getData():
-                    uri = intent.getData()
-                    path = self.copy_uri_to_internal_storage(uri)
-                    self.update_ui(path, "Intent (Copied)")
-            elif requestCode == 3000: # Галерея (прямой URI)
-                if intent and intent.getData():
-                    uri = intent.getData()
-                    self.update_ui(uri.toString(), "Intent (Direct URI)")
-
-    def copy_uri_to_internal_storage(self, uri):
-        """Ручное копирование данных из URI в файл приложения"""
+    # --- МЕТОД 1: Копирование в файл приложения ---
+    def method_stream_copy(self, uri):
         try:
-            context = PythonActivity.mActivity
-            filename = f"copy_{int(os.times().elapsed)}.jpg"
-            dest_path = os.path.join(self.user_data_dir, filename)
-            input_stream = context.getContentResolver().openInputStream(uri)
-            output_stream = autoclass('java.io.FileOutputStream')(dest_path)
+            cr = PythonActivity.mActivity.getContentResolver()
+            path = os.path.join(self.user_data_dir, "method1.jpg")
             
-            buffer = autoclass('java.lang.reflect.Array').newInstance(autoclass('java.lang.Byte').TYPE, 4096)
+            in_stream = cr.openInputStream(uri)
+            out_stream = FileOutputStream(path)
+            
+            # Стандартный буфер
+            buffer = bytearray(1024 * 64)
             while True:
-                read = input_stream.read(buffer)
+                read = in_stream.read(buffer)
                 if read == -1: break
-                output_stream.write(buffer, 0, read)
+                out_stream.write(buffer[:read])
             
-            input_stream.close()
-            output_stream.close()
-            return dest_path
-        except Exception as e: return f"Error: {e}"
+            in_stream.close()
+            out_stream.close()
+            
+            self.root.ids.img_method_1.source = path
+            self.root.ids.img_method_1.reload()
+        except Exception as e:
+            Logger.error(f"Method 1 Error: {e}")
 
-    def copy_file_to_app_folder(self, source_path):
-        """Копирование существующего файла"""
-        dest = os.path.join(self.user_data_dir, os.path.basename(source_path))
-        shutil.copy2(source_path, dest)
-        return dest
+    # --- МЕТОД 2: Загрузка в память без сохранения на диск ---
+    def method_core_image(self, uri):
+        try:
+            cr = PythonActivity.mActivity.getContentResolver()
+            in_stream = cr.openInputStream(uri)
+            
+            # Читаем все байты сразу
+            from jnius import cast
+            # Используем вспомогательный метод для чтения всех байтов
+            # В Python это проще сделать через цикл, как выше, но в BytesIO
+            data_io = io.BytesIO()
+            buffer = bytearray(1024 * 64)
+            while True:
+                read = in_stream.read(buffer)
+                if read == -1: break
+                data_io.write(buffer[:read])
+            in_stream.close()
+            
+            data_io.seek(0)
+            # Создаем текстуру из байтов
+            core_img = CoreImage(data_io, ext="jpg")
+            self.root.ids.img_method_2.texture = core_img.texture
+        except Exception as e:
+            Logger.error(f"Method 2 Error: {e}")
 
-    def update_ui(self, path, method_name):
-        """Обновление картинки и текста на экране"""
-        if path:
-            self.root.ids.display_img.source = path
-            self.root.ids.display_img.reload()
-            self.root.ids.status_label.text = f"Метод: {method_name}\nПуть: {path}"
-
-    def clear_local_files(self):
-        """Очистка всех созданных фото из папки приложения"""
-        for f in os.listdir(self.user_data_dir):
-            if f.endswith(".jpg") or f.endswith(".png"):
-                os.remove(os.path.join(self.user_data_dir, f))
-        self.root.ids.display_img.source = "assets/placeholder.png"
-        self.root.ids.status_label.text = "Кэш очищен"
+    # --- МЕТОД 3: Попытка взять реальный путь (обычно выдает ошибку доступа на Android 11+) ---
+    def method_direct_path(self, uri):
+        # На новых Android URI выглядит как content://... и через os.path.exists не читается
+        # Этот метод покажет, почему "простой" способ не работает
+        uri_string = uri.toString()
+        try:
+            # Пытаемся подсунуть URI напрямую в Image
+            self.root.ids.img_method_3.source = uri_string
+        except Exception as e:
+            Logger.error(f"Method 3 Error: {e}")
 
 if __name__ == '__main__':
-    TestApp().run()
+    ImageTestApp().run()
